dbf5910c99ac388ad216b3ae515ad8887935594b
billniakas <billniakas@gmail.com>
2017-07-21 00:32:09 +0300

    Fixed wrong inputs

--- interfacelift.py
+++ interfacelift.py
@@ -9,7 +9,7 @@ Input = subprocess.getoutput("xrandr | grep -i '*'")
 resolution=Input.split()[0]
 j=1
 print("Your screen resolution is",resolution)
-print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+
 while True:
     def rss_feed(url):
         url += "widescreen/"+str(resolution)+"/index"+str(j)+".html"
@@ -27,27 +27,19 @@ while True:
             return 0
         except urllib.error.URLError as e:
             print(e)
-            if hasattr(e, 'reason'):  # χωρίς σύνδεση ιντερνετ
+            if hasattr(e, 'reason'):  # no internet connection
                 print('Failed to connect to the server')
                 print('Reason : ', e.reason)
             return 0
-    ##    else:
-    ##        if (process_feed(filename)) : return 1
-    ##        else : return 0
+
 
     def process_feed(filename):
             
         with open(filename, 'r', encoding = 'utf-8') as f:
             rss = f.read().replace("\n", " ")
-            #feeds = []
-            #items = re.findall(r'<b style="color: #bb2f0e;">([0-9]{2,})</b>.</p>',rss, re.I)
-
-            #print ("These wallpapers extend in",items[0],"pages")
-           
             wallpapers = re.findall(r'<a href="(/wallpaper/\w{7}/\w{5,}.jpg)',rss, re.I)
             
             for i in wallpapers:
-               
                 print("\nDownloading wallpaper",i[19:])
                 wallpaper = wget.download('https://interfacelift.com'+i)
             
@@ -60,27 +52,34 @@ while True:
         rss = f.read().replace("\n", " ")
         feeds = []
         items = re.findall(r'<b style="color: #bb2f0e;">([0-9]{2,})</b>.</p>',rss, re.I)
-
         print ("\nThese wallpapers extend in",items[0],"pages")
-
-
-    pages=input("How many pages of the "+items[0]+" do you want to download ? \nPress enter if you want to quit : ")
-    if pages=="": break
-    else:
         
-
-        if os.path.exists(resolution) is True:
-            pass
-        else:
-            path = os.mkdir(resolution)
-        dir = str(os.getcwd()+"/"+str(resolution))
-        os.chdir(dir)
-        for j in range(1,int(pages)+1):
+    while True:
+        try:
+            pages=input("How many pages of the "+items[0]+" do you want to download ? \nPress enter if you want to quit : ")
+            if int(pages) not in range(1,int(items[0])+1):
+                print("Number of pages not in range")
+                continue
             
-            rss_feed(url)
-            process_feed(filename)
-
-        print("\n")
+            else:
+                if os.path.exists(resolution) is True:
+                    pass
+                else:
+                    path = os.mkdir(resolution)
+                dir = str(os.getcwd()+"/"+str(resolution))
+                os.chdir(dir)
+                for j in range(1,int(pages)+1):
+                    
+                    rss_feed(url)
+                    process_feed(filename)
+                    
+                print("\n")
+                break
+        except ValueError as e:
+            if pages=="":
+                break
+            else:print("Error",e)
+        
     break
 
 os.remove("tempfile.rss")
